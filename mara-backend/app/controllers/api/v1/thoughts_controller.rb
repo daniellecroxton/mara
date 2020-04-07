f73d99f4dbf604e83424d2616bb37d55630c1483
daniellecroxton <danikrist@gmail.com>
2020-04-07 09:47:00 -0600

    added basic methods to thoughts controller

--- mara-backend/app/controllers/api/v1/thoughts_controller.rb
+++ mara-backend/app/controllers/api/v1/thoughts_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::ThoughtsController < ApplicationController
+
+def index
+    @thoughts = Thought.all
+    render json: @thoughts
+end
+
+def create
+    @thought.new(thought_params)
+    if @thought.save
+        render json: @thought, status: :accepted
+    else
+        render json: { errors: @thought.errors.full_messages }, status: :unprocessible_entity
+    end
+end
+
+private
+
+def thought_params
+    params.permit(:text, :user)
+end
+
 end
