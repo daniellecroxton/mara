3c0f2328c7db3641353a338a548d9ebe79851961
daniellecroxton <danikrist@gmail.com>
2020-04-08 21:22:22 -0600

    Created seed data

--- mara-backend/db/seeds.rb
+++ mara-backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test_thoughts = [
+    'What will happen if I lose my job?',
+    "I'm afraid this won't work out.",
+    'Working from home will drive me crazy.',
+    "I can't take another day of homeschooling my kids!",
+    'What if I get sick?',
+    "I hate that my body doesn't work like it used to",
+    'I just want to eat candy!!!!',
+    'I am unhappy. :('
+]
+
+test_thoughts.each do | thought |
+    Thought.create(text: thought)
+end
