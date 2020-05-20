32172d01bb3fafc4e819a03e24884caf16cfbe1f
daniellecroxton <danikrist@gmail.com>
2020-05-19 21:30:54 -0600

    removed user column from thoughts table

--- mara-backend/db/schema.rb
+++ mara-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200520025654) do
+ActiveRecord::Schema.define(version: 20200520032938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20200520025654) do
 
   create_table "thoughts", force: :cascade do |t|
     t.string "text"
-    t.string "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
